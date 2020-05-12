6f66aa70ab5e5be0c28d8beae6d4ac17ed5ec741
Luthfi Alhavizd <alhavizd.luthfi@gmail.com>
2020-05-12 16:33:15 +0700

    setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import router from './router'
 import vuetify from './plugins/vuetify'
+import i18n from './plugins/i18n'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
